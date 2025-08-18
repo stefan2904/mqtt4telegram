8751565a8d76940f3e8282cb9cca6e2d7f6ab105
Stefan <dev@2904.cc>
2025-08-18 20:46:54 +0200

    fix libcoap install

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM python:3
 
+RUN apt update \
+	&& apt install libssl3 libssl-dev \
+	&& apt autoremove && apt autoclean
+
 ADD requirements.txt /
 ADD tools/install-coap-client.sh /
 RUN sh install-coap-client.sh
--- tools/install-coap-client.sh
+++ tools/install-coap-client.sh
@@ -4,6 +4,7 @@
 git clone --depth 1 --recursive -b dtls https://github.com/obgm/libcoap.git
 cd libcoap
 ./autogen.sh
-./configure --disable-documentation --disable-shared --without-debug CFLAGS="-D COAP_DEBUG_FD=stderr"
+./configure --disable-dtls --disable-tinydtls --without-submodule-tinydtls --disable-examples --disable-documentation --without-debug CFLAGS="-D COAP_DEBUG_FD=stderr"
 make
 make install
+
